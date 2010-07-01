08ba20a6802b7dc152ade3268478a58963246903
Martin Svalin <martin.svalin@newsdesk.se>
2010-07-01 22:32:16 +0200

    changed route to registrations#new

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Codemocracy::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "registrations#index"
+  root :to => "registrations#new"
 
   # See how all your routes lay out with "rake routes"
 
