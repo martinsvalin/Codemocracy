3aeb6c8787adacbf672b7aa70480b04d2f4587f6
Martin Svalin <martin.svalin@newsdesk.se>
2010-07-11 15:19:36 +0200

    Removed safe_t helper, in favor of haml "!=" syntax. Changed format and doctype to XHTML5.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def safe_t(*translation_keys)
-    (t *translation_keys).html_safe
-  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Codemocracy::Application.initialize!
+
+Haml::Template.options = {
+  :format => :xhtml, 
+  :escape_html => true,
+  :attr_wrapper => '"',
+  # :autoclose => %w(meta img link br hr input area param col base),
+  :preserve => %w(textarea pre)
+} 
