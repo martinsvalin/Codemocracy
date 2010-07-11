711bcd4194a0fb439595b61ee5b569c0d9ea079c
Martin Svalin <martin.svalin@newsdesk.se>
2010-07-11 15:20:23 +0200

    Specified autoclosing elements for haml.

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,6 @@ Haml::Template.options = {
   :format => :xhtml, 
   :escape_html => true,
   :attr_wrapper => '"',
-  # :autoclose => %w(meta img link br hr input area param col base),
+  :autoclose => %w(meta img link br hr input area param col base),
   :preserve => %w(textarea pre)
 } 
