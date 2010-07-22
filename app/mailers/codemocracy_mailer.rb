e559416da02bb401f0ee177cf49a51dd841826c2
Martin Svalin <martin.svalin@newsdesk.se>
2010-07-22 10:25:15 +0200

    Renamed RegistrationMailer to CodemocracyMailer

---
+++ app/mailers/codemocracy_mailer.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+class CodemocracyMailer < ActionMailer::Base
+  default :from => "martin@codemocracy.se"
+  default_url_options[:host] = "codemocracy.se"
+  
+  def registered(registration)
+      @registration = registration
+      @contact_email = "codemocracy@codemocracy.se"
+      mail(:to => registration.email,
+           :subject => "Du är nu anmäld till Codemocracy")
+    end
+  
+end
