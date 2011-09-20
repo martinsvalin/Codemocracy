b3746ffbe561c432470312046e0bc13ccaf74235
Martin Svalin <martin@lite.nu>
2011-09-20 21:54:17 +0200

    Added pages from Codemocracy 2010

---
+++ app/controllers/projects2010_controller.rb
@@ -0,0 +1,40 @@
+class Projects2010Controller < ApplicationController
+  def index
+  end
+
+  def valkoll
+  end
+
+  def lokalpolitik
+  end
+
+  def myndighetskoll
+  end
+
+  def kollektivt
+  end
+
+  def riksdagen_pa_facebook
+  end
+
+  def engagemangsindex
+  end
+
+  def boreda_at_codemocracy
+  end
+
+  def crime_tracker
+  end
+
+  def tagtider
+  end
+
+  def riksdagen_i_ply
+  end
+
+  def georapport
+  end
+
+  def edemokrat
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Codemocracy::Application.routes.draw do
   post "users/create", :as => :users
 
+  # Legacy routes
+  get "projects",                       :to => "projects2010#index"
+  get "projects/valkoll",               :to => "projects2010#valkoll"
+  get "projects/lokalpolitik",          :to => "projects2010#lokalpolitik"
+  get "projects/myndighetskoll",        :to => "projects2010#myndighetskoll"
+  get "projects/kollektivt",            :to => "projects2010#kollektivt"
+  get "projects/riksdagen_pa_facebook", :to => "projects2010#riksdagen_pa_facebook"
+  get "projects/engagemangsindex",      :to => "projects2010#engagemangsindex"
+  get "projects/boreda_at_codemocracy", :to => "projects2010#boreda_at_codemocracy"
+  get "projects/crime_tracker",         :to => "projects2010#crime_tracker"
+  get "projects/tagtider",              :to => "projects2010#tagtider"
+  get "projects/riksdagen_i_ply",       :to => "projects2010#riksdagen_i_ply"
+  get "projects/georapport",            :to => "projects2010#georapport"
+  get "projects/edemokrat",             :to => "projects2010#edemokrat"
+
   root :to => "welcome#index"
 end
