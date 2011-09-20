2a4978771e2771a95cc48f09062e989839da3f61
Martin Svalin <martin@lite.nu>
2011-09-20 20:03:55 +0200

    Better flash success message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new params[:user]
     if @user.save
-      redirect_to root_path, :notice => "Fint!"
+      redirect_to root_path, :notice => "Du är nu anmäld med adressen #{@user.email}. Trevligt!"
     else
       redirect_to root_path, :error => "Fan också"
     end
